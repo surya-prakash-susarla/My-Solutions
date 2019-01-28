e07c129bba9c206c7cb1752eadf19aefbefee647
Surya-06 <susarla.surya.prakash.1998@gmail.com>
2019-01-28 11:39:22 +0530

    Balanced Forest ( failing 3 cases )

---
+++ LIB_big_num.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class big_int {
+    public:
+        string value ;
+
+        big_int(){
+            value = "";
+        };
+
+        big_int ( string value ){
+            this->value = value;
+        };
+
+        void get ( string value ){
+            this->value = value;
+            return;
+        };
+
+        big_int operator+ ( const big_int &right_value ){
+            string lvalue = this->value, rvalue = right_value.value;
+            vector<char> result ;
+            reverse ( lvalue.begin() , lvalue.end() );
+            reverse ( rvalue.begin() , rvalue.end() );
+            int diff = lvalue.size() - rvalue.size() ;
+            if ( diff > 0 ){
+                string zeroes ( diff , '0' );
+                rvalue = rvalue + zeroes;
+            }
+            else if ( diff < 0 ){
+                string zeroes ( 0-diff , '0' );
+                lvalue = lvalue + zeroes;
+            }
+            int i=0,j=0,increment=0;
+            while ( i<lvalue.size() and j<rvalue.size() ){
+                int answer = (lvalue[i++]-'0')+(rvalue[j++]-'0')+increment;
+                result.push_back(char(answer%10+'0'));
+                increment = answer / 10 ;
+            }
+            if ( increment > 0 )    result.push_back(char(increment+'0'));
+            reverse ( result.begin() , result.end() );
+            string final_result  (result.begin(),result.end());
+            big_int temp (final_result);
+            return temp;
+        };
+
+        big_int operator- ( const big_int &right_value ){
+            string lvalue = this->value, rvalue = right_value.value;
+            vector<char> result;
+            reverse( lvalue.begin() , lvalue.end() );
+            reverse( rvalue.begin() , rvalue.end() );
+            int diff = lvalue.size() - rvalue.size() ;
+            bool pos_result = true ;
+            if ( diff > 0 ){
+                string zeroes ( diff , '0' );
+                rvalue = rvalue + zeroes ;
+            }
+            else if ( diff < 0 ){
+                pos_result = false; 
+                string zeroes ( 0-diff , '0' );
+                lvalue = lvalue + zeroes ;
+            }
+
+            cout << "lvalue = " << lvalue << endl;
+            cout << "rvalue = " << rvalue << endl;
+
+            int i=0 , j=0 ;
+            bool borrow = false ;
+            while ( i<lvalue.size() and j<rvalue.size() ){
+                int top_digit = lvalue[i]-'0' , low_digit = rvalue[i]-'0' ;
+                if ( borrow ){
+                    top_digit-=1 ;
+                    borrow = false;
+                }
+                if ( top_digit < low_digit ){
+                    top_digit += 10;
+                    borrow = true ;
+                }
+                int answer = top_digit - low_digit; 
+                result.push_back(char(answer+'0'));
+                i++ , j++ ;
+            }
+            if ( borrow )   result.push_back('-');
+            reverse(result.begin(),result.end());
+            string final_result(result.begin(), result.end());
+            big_int temp(final_result);
+            return temp;
+        };
+
+        big_int single_char_mul ( string input , char mul_dig ){
+            if ( mul_dig == '0' )   return string(input.size(),'0');
+            vector<char> final_answer ;
+            reverse ( input.begin() , input.end() );
+            int i=0 , digit = mul_dig - '0' , increment = 0 ;
+            while ( i<input.size() ){
+                int answer = ( (input[i]-'0') * digit ) + increment ;
+                if ( answer >= 10 ) {
+                    increment = answer / 10 ;
+                    answer = answer % 10 ;
+                }
+                else    increment = 0;
+                final_answer.push_back ( char(answer+'0') );
+                i++;
+            }
+            if(increment >0)    final_answer.push_back(char(increment+'0'));
+            reverse ( final_answer.begin() , final_answer.end() );
+            string final_result (final_answer.begin(),final_answer.end());
+            big_int temp(final_result);
+            return temp;
+        }
+
+        big_int operator* ( const big_int &right_value ){
+            string lvalue = this->value , rvalue = right_value.value;
+            reverse(rvalue.begin(),rvalue.end());
+            vector<big_int> values;
+            int i=0;
+            while ( i<rvalue.size() ){
+                char digit = rvalue[i];
+                big_int current_value = single_char_mul ( lvalue , digit );
+                string zeroes (i , '0');
+                string solution = current_value.value + zeroes;
+                big_int temp(solution);
+                values.push_back(temp);
+                i++;
+            }
+            big_int init = values[0];
+            for ( int i=1 ; i<values.size() ; i++ ){
+                init = init+values[i];
+            }
+            return init;
+        };
+
+        big_int min ( big_int value ){
+            if ( this->value.size() < value.value.size() )  return *this;
+            else if ( this->value.size() > value.value.size() ) return value;
+            else {
+                int i=0 ;
+                while ( i<this->value.size() ){
+                    if ( this->value[i]-'0' < value.value[i]-'0' )  return *this;
+                    else if ( this->value[i]-'0' > value.value[i]-'0' ) return value;
+                    i++;
+                }
+                // both numbers are equal
+                return value;
+            }
+            return value;
+        };
+};
+
+/*
+int main() {
+    big_int a,b;
+    string input;
+    cin >> input ;
+    a.get(input);
+    cout << "Value of a = " << a.value << endl;
+    cin >> input;
+    b.get(input);
+    cout << "Value of b = " << b.value << endl;
+    
+    cout << "starting mul "<<endl;
+    big_int temp_sub  = a*b;
+    cout << temp_sub.value << endl;
+    return 0;
+}
+*/
---
+++ balancedForest.cpp
@@ -0,0 +1,189 @@
+
+#include <bits/stdc++.h>
+
+#include <vector>
+#include <stack>
+#include <limits>
+
+using namespace std;
+
+typedef long long int llint;
+
+vector<string> split_string(string);
+
+llint max_value = numeric_limits<llint>::max();
+
+llint dfs(llint node, vector<bool> &visited, vector<vector<bool>> matrix, vector<int> cost)
+{
+    stack<llint> stack_values;
+    llint total_sum = 0;
+    stack_values.push(node);
+    while (not stack_values.empty())
+    {
+        llint current_node = stack_values.top();
+        stack_values.pop();
+        total_sum += cost[current_node];
+        visited[current_node] = true;
+        for (llint i = 0; i < matrix.size(); i++)
+            if (matrix[current_node][i] and not visited[i])
+            {
+                // cout  << "Pushing into stack " << i << endl;
+                stack_values.push(i);
+            }
+    }
+    return total_sum;
+}
+
+// Complete the balancedForest function below.
+llint balancedForest(vector<int> c, vector<vector<int>> edges)
+{
+    cout << endl
+         << endl
+         << "-------------------------------" << endl;
+    llint nodes = c.size();
+    vector<vector<bool>> matrix(nodes, vector<bool>(nodes, false));
+    for (llint i = 0; i < edges.size(); i++)
+        matrix[edges[i][0] - 1][edges[i][1] - 1] = true, matrix[edges[i][1] - 1][edges[i][0] - 1] = true;
+    llint total_sum = 0;
+    for (int i = 0; i < nodes; i++)
+        total_sum += c[i];
+    cout << "Total dfs value : " << total_sum << endl;
+    llint final_answer = max_value, n_edges = edges.size();
+    for (llint i = 0; i < n_edges; i++)
+    {
+        vector<bool> visited(nodes, false);
+        llint v1 = edges[i][0] - 1, v2 = edges[i][1] - 1;
+        matrix[v1][v2] = false, matrix[v2][v1] = false;
+        llint sum1 = dfs(v1, visited, matrix, c);
+        // check possibility of removing only a single edge
+        if (total_sum - sum1 == sum1 and sum1 < final_answer)
+            final_answer = sum1;
+        //else if ( sum1 < total_sum - sum1 )
+        //{
+        for (llint j = i + 1; j < n_edges; j++)
+        {
+            vector<bool> visited_backup(visited);
+            llint s1_bk = sum1;
+            llint v3 = edges[j][0] - 1, v4 = edges[j][1] - 1;
+            matrix[v3][v4] = false, matrix[v4][v3] = false;
+            llint sum2 = -1, sum3 = -1;
+            if (visited[v3])
+            {
+                // cout  << "Picked second " << v4 << endl;
+                vector<bool> temp_visited(nodes, false);
+                sum2 = dfs(v4, temp_visited, matrix, c);
+                sum1 -= sum2;
+                sum3 = total_sum - sum1 - sum2;
+            }
+            else if (sum1 < total_sum - sum1)
+            //else
+            {
+                // cout  << "Picked second direct : " << v3 << endl;
+                sum2 = dfs(v3, visited, matrix, c);
+                sum3 = total_sum - sum1 - sum2;
+            }
+            if (sum1 == sum2 and sum3 < sum2 and sum2 - sum3 < final_answer)
+                final_answer = sum2 - sum3;
+            else if (sum2 == sum3 and sum1 < sum2 and sum3 - sum1 < final_answer)
+                final_answer = sum3 - sum1;
+            else if (sum3 == sum1 and sum2 < sum1 and sum1 - sum2 < final_answer)
+                final_answer = sum1 - sum2;
+            matrix[v3][v4] = true, matrix[v4][v3] = true;
+            visited = visited_backup;
+            sum1 = s1_bk;
+            // cout  << v1 << " " << v2 << " " << v3 << " " << v4 << endl;
+            // cout  << sum1 << " " << sum2 << " " << sum3 << endl;
+        }
+        //}
+        matrix[v1][v2] = true, matrix[v2][v1] = true;
+    }
+    if (final_answer == max_value)
+        return -1;
+
+    cout << final_answer << endl;
+
+    return final_answer;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++)
+    {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string c_temp_temp;
+        getline(cin, c_temp_temp);
+
+        vector<string> c_temp = split_string(c_temp_temp);
+
+        vector<int> c(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            int c_item = stoi(c_temp[i]);
+
+            c[i] = c_item;
+        }
+
+        vector<vector<int>> edges(n - 1);
+        for (int i = 0; i < n - 1; i++)
+        {
+            edges[i].resize(2);
+
+            for (int j = 0; j < 2; j++)
+            {
+                cin >> edges[i][j];
+            }
+
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        int result = balancedForest(c, edges);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string)
+{
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ')
+    {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos)
+    {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
