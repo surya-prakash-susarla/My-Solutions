e0b8e0054eb7dd4888a53bff5cf3297dbd87f701
Surya-06 <susarla.surya.prakash.1998@gmail.com>
2019-04-03 22:57:42 +0530

    O(mn) time and O(1) space'

---
+++ battleShipsInABoard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int answer = 0 ;
+        for ( int i=0 ; i<board.size() ; i++ )
+            for ( int j=0 ; j<board[i].size() ; j++ )
+                // check left 
+                if ( j-1 >=0 and board[i][j-1] != '.' )
+                    continue;
+                else if ( i-1 >= 0 and board[i-1][j] != '.' )
+                    continue;
+                else if ( board[i][j] != '.' )
+                    answer += 1;
+        return answer;
+    }
+};
