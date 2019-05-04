eb89b81ca95305b7012e7e32ecabc66b19a6835a
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2019-05-04 19:51:37 +0530

    good greedy thanos snap

---
+++ brokenCalculator.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int brokenCalc( int input , int target ) {
+        int moves = 0 ;
+        while ( target > input ) {
+            if ( target % 2 == 1 )  {
+                // odd numbered target value 
+                target += 1;
+                moves += 1;
+            }
+            target = target / 2;
+            moves += 1;
+        }
+        moves += ( input-target );
+        return moves;
+    }
+};
