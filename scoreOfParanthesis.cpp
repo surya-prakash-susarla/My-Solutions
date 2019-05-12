ad186c52268df9e45926ec70f5e0aa784c0e5d86
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2019-05-12 21:10:26 +0530

    string paranthesis recrsive

---
+++ scoreOfParanthesis.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    pair<int,int> calc ( int index , const string& input ) {
+        index+=1 ;
+        int score = 0;
+        while ( input[index] != ')' ) {
+            pair<int,int> ret_value = calc ( index , input );
+            score += ret_value.first ;
+            index = ret_value.second + 1 ;
+        }
+        if ( score == 0 )   return {1,index}; 
+        return {2*score,index}; 
+    }
+    
+    int scoreOfParentheses(string input) {
+        int index = 0 , score = 0 ;
+        while ( index < input.size() ) {
+            pair<int,int> ret_value = calc ( index , input );
+            index  = ret_value.second+1 , score += ret_value.first ;
+        }
+        return  score ; 
+    }
+};
