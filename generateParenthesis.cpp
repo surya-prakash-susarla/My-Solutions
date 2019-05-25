e5c644362c2abd04caa41e7bf374fe5d66d047dc
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2019-05-25 19:31:37 +0530

    simple backtracking

---
+++ generateParenthesis.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    vector<string> answer ;
+    
+    void calc ( int pairs , int rem , string value ) {
+        if ( pairs == 0 ) {
+            value += string ( rem , ')' );
+            answer.push_back ( value );
+            return;
+        }
+        if ( rem != 0 )
+            calc ( pairs , rem-1 , value + ')' );
+        calc ( pairs-1 , rem+1 , value + '(' );
+        return;
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        calc ( n , 0 , string() );
+        return answer ;
+    }
+};
