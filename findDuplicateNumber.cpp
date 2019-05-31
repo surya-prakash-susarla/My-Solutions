01eb9d03dd90f8aa78a530150ef618795f149478
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2019-05-31 12:22:35 +0530

    other med ones getting closer to 100

---
+++ findDuplicateNumber.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<bool> values ( nums.size() , false );
+        for ( int i : nums )
+            if ( not values[i] )    values[i] = true ;
+            else    return i;
+        return -1;
+    }
+};
