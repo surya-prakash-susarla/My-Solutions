012d671fba56433410cd59edb84a2c8af35c2af9
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2019-05-31 10:41:21 +0530

    first one simple backtrack with map, tree based

---
+++ mostFrequentSubtreeSum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+#define node TreeNode 
+
+class Solution {
+public:
+    
+    int calc ( node* root , map<int,int>& values ) {
+        if ( root == NULL ) return 0; 
+        int current = calc(root->left,values) + calc(root->right,values) + root->val ;
+        values[current]+=1;
+        return current;
+    }
+    
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        map<int,int> values ;
+        calc ( root , values );
+        int freq = -1 ;
+        for ( auto it : values )    if ( it.second > freq )    freq = it.second ;
+        vector<int> answer ;
+        for ( auto it : values )
+            if ( it.second == freq )
+                answer.push_back ( it.first );
+        return answer;
+    }
+};
