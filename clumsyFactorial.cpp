0ef035deea3d4152e69b5635cfe8563a746e1fba
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2019-06-03 22:05:40 +0530

    iteration, modified factorial

---
+++ clumsyFactorial.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    int ins ( int index ) {
+        if ( index == 1 )  return 1; 
+        else if ( index == 2 )  return 2*1 ;
+        return  ( index * (index-1) ) / ( index-2 );
+    }
+    
+    int clumsy( int limit ) {
+        int answer = ins ( limit );
+        bool add = true ;
+        for ( int i = limit -3 ; i > 0 ; i-- )
+            if ( add ) {
+                answer += i ; 
+                add = false ;
+            }
+            else {
+                answer = answer - ins ( i ) ;
+                i -= 2 ;
+                add = true ;
+            }
+        return answer;
+    }
+};
