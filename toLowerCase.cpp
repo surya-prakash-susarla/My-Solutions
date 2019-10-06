97f8bf0bfa7c7e85ab5e285363a0ea0e5fbf64a1
Surya Prakash <susarla.surya.prakash.1998@gmail.com>
2019-10-06 20:45:33 +0530

    lower case simple test

---
+++ toLowerCase.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string toLowerCase(string input) {
+        for ( int i=0 ; i<input.size() ; i++ )
+            if ( input[i] >= 'A' and input[i] <= 'Z' )
+                input[i] = char ( (int)input[i] + (int)'a' - (int)'A');
+        return input ;
+    }
+};
