a1c4a3a768da35853d1108e4d732f63d16461c8d
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2020-08-30 07:53:20 +0100

    new version of high buildings g++ errors in vector

--- round e 20/highBuildings.cpp
+++ round e 20/highBuildings.cpp
@@ -1,52 +1,88 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <limits>
 
-using namespace std;
+typedef long long int llint;
 
-typedef long long int llint ;
+llint n , a , b , c ;
 
+constexpr llint A_VISIT = -1 ; 
+constexpr llint B_VISIT = -2 ;
+constexpr llint INVIS = -3 ;
+constexpr llint COMMON = -4 ;
 
-
-
-vector<llint> __calc__ (void) {
-	llint n , a, b ,c ; 
-	cin >> n >> a >> b >> c ; 
-
+std::vector<llint> __calc__ () {
+	std::cin >> n >> a >> b >> c ;
+	
 	llint only_a = a - c ;
 	llint only_b = b - c ;
-	llint empty = n - ( only_a + only_b + c ) ; 	
+	llint empty = n - ( a + b + c );
+
+	if ( n - ( only_a + only_b ) > c )
+		return std::vector<llint>();
 	
+	std::vector<llint> answer ( n , COMMON );
+	for ( llint i=0 ; i<only_a ; i++ )
+		answer[i] = A_VISIT ; 
+	for ( llint i=only_b ; i<n ; i++ )
+		answer[i] = B_VISIT ;
+
+	llint start_limit = only_a ; 
+	llint end_limit = only_b - 1 ;
 	
+	if ( only_b == n ){
+		llint i = end_limit - 1 ; 
+		while ( empty-- > 0 )
+			answer[i--] = INVIS;
+	} else {
+		llint i = start_limit + 1 ;
+		while ( empty-- > 0 )
+			answer[i++ ] = INVIS;
+	}
+
+	llint common_height = n ; 
+	llint ind_height = n-1 ; 
+	llint invis = n-2 ;
+
+	for ( llint &i : answer ){
+		if ( i == A_VISIT || i == B_VISIT )
+			i = ind_height ;
+		else if ( i == COMMON )
+			i = common_height ;
+		else 
+			i = invis ;
+	}
+
+	return answer;
+}	
 
 
-	return vector<llint>();
-}
-
-void __print__ ( const vector<llint>& answer ) {
+void __print__ ( std::vector<llint> answer ) {
 	if ( answer.empty() ){
-		cout << "IMPOSSIBLE" ; 
+		std::cout << "IMPOSSIBLE" ; 
 		return;
 	}
-	for ( size_t i=0 ; i<answer.size() ; i++ ){
-		cout << answer[i] ;
-		if ( i < answer.size() -1 ) 
-			cout << endl;
+	for ( llint i=0 ; i<n ; i++ ){
+		std::cout << answer[i] ; 
+		if ( i < n-1 )
+			std::cout <<  " " ;
 	}
 	return;
 }
 
 
-int main() {
+int main(){
 	llint t;
-	cin >> t; 
+	std::cin >> t;
 	for ( llint i=1 ; i<=t ; i++ ){
-		vector<llint> answer = __calc__ () ; 
-		cout << "Case #" << i << ": " ; 
+		std::vector<llint> answer = __calc__ ();
+
+		std::cout << "Case #" << i << ": " ;	
+
 		__print__ ( answer ) ; 
-		if ( i < t  )
-			cout << endl ;;
+
+		if ( i < t )
+			std::cout << std::endl;
 	}
 	return 0;
 }
