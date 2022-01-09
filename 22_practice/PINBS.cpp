2c270d745be4a012ff2af4367abbd8477110235b
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2022-01-09 11:16:49 +0530

    easy one again

---
+++ 22_practice/PINBS.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <utility>
+#include <map>
+#include <stack>
+#include <memory>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <tuple>
+
+#define in scanf
+#define out printf
+
+using namespace std;
+
+// Problem: 
+
+typedef long long int llint;
+typedef long double ldouble;
+
+bool __calc__() {
+  std::string value ; 
+  cin >> value ; 
+
+  if ( value.size() == 1 ) return false ;
+
+  reverse(value.begin(), value.end());
+
+  // cout << value[0] << endl;
+  // cout << value[1] << endl;
+
+  return std::any_of(value.begin()+1, value.end(), [](const char& c){
+    return c=='1';
+  });
+
+}
+
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  
+  llint t ; 
+  cin >> t;
+  for ( llint i=0 ; i<t ; i++ ) { 
+    cout << (__calc__() ? "Yes" : "No") ; 
+    if ( i < t-1 )  cout << endl;
+  }
+
+  return 0;
+}
