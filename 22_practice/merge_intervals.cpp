fb7feb12310bc66667169c1d3513fc76a40db0b8
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2022-04-16 14:35:26 +0000

    basic merge intervals problem

---
+++ 22_practice/merge_intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
+            if ( a[0] != b[0] )  return a[0] < b[0];
+            return a[1] < b[1] ;
+        });
+        
+        vector<vector<int>> answer;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        
+        for ( int i=1 ; i<intervals.size() ; i++ ){
+            if (intervals[i][0] <= end) {
+                end = max(end, intervals[i][1]);
+            } else {
+                answer.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+  
+        answer.push_back({start ,end });
+        
+        return answer;
+    }
+};
