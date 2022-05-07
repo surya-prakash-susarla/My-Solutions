4acec58cc1d7c4fe632c475c0c24c027d6e263cf
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2022-05-07 12:49:47 +0000

    clang format recent file

--- 22_practice/kth_missing_positive_number.cpp
+++ 22_practice/kth_missing_positive_number.cpp
@@ -1,17 +1,19 @@
 class Soluton {
-public:
-    int findKthPositive(vector<int>& values, int k) {
-        int orig = k ;
-        
-        k -= (values[0] - 1);
-        if ( k <= 0 )   return orig;
-        
-        for(int i=1 ; i<values.size(); i++){
-            orig = k ;
-            k -= (values[i] - values[i-1] - 1);
-            if ( k <= 0 )   return values[i-1] + orig;
-        }
-        
-        return *values.rbegin() + k;
+ public:
+  int findKthPositive(vector<int>& values, int k) {
+    int orig = k;
+
+    k -= (values[0] - 1);
+    if (k <= 0)
+      return orig;
+
+    for (int i = 1; i < values.size(); i++) {
+      orig = k;
+      k -= (values[i] - values[i - 1] - 1);
+      if (k <= 0)
+        return values[i - 1] + orig;
     }
+
+    return *values.rbegin() + k;
+  }
 };
