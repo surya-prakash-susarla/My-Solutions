b7c9aa601bbd1239305111c10e3f477058b29ab9
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-01-29 23:50:15 -0500

    count distinct numbers on board - easy

---
+++ 23_practice/count_distinct_numbers_on_board.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <limits>
+#include <queue>
+#include <cmath>
+#include <list>
+#include <string>
+#include <cstdio>
+#include <numeric>
+#include <stack>
+
+typedef long long int llint;
+typedef long long unsigned int ullint;
+
+using namespace std;
+
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        if ( n == 1 )   return 1;
+        return n-1;
+    }
+};
+
+int main() {
+
+    ios_base::sync_with_stdio(false); 
+
+
+    return 0;
+}
+
