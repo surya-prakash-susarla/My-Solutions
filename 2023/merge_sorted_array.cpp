09ddd947d8a2902a56678e269ad167b35f307f24
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-07-28 18:40:02 -0700

    merge sorted array - easy - 150 track

---
+++ 2023/merge_sorted_array.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+typedef long long int llint;
+typedef long long unsigned int ullint;
+
+using namespace std;
+
+class Solution {
+ public:
+  void merge(vector<int>& values, int m, vector<int>& values2, int n) {
+    for (int i = 0; i < n; i++)
+      values[m + i] = values2[i];
+    sort(values.begin(), values.end());
+    return;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  return 0;
+}
