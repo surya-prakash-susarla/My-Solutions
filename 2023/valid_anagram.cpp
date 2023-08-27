bc5b68bf20acd638446092fb82a5c2ada6f15441
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-08-27 02:40:57 -0400

    Valid Anagram - easy - 150 track

---
+++ 2023/valid_anagram.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+typedef long long int llint;
+typedef long long unsigned int ullint;
+
+using namespace std;
+
+class Solution {
+ public:
+  bool isAnagram(string a, string b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    return a == b;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  return 0;
+}
