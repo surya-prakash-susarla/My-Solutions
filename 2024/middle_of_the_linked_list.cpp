400cc108701b1d53efa207fcccf2a8281b4f9258
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2024-03-07 16:09:52 -0500

    Middle of the Linked List - easy - daily

---
+++ 2024/middle_of_the_linked_list.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+typedef long long int llint;
+typedef long long unsigned int ullint;
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* middleNode(ListNode* head) {
+    typedef ListNode node;
+
+    node* temp = head;
+    node* temp2 = head;
+
+    while (temp2 and temp2->next) {
+      temp = temp->next;
+      temp2 = temp2->next;
+      if (temp2)
+        temp2 = temp2->next;
+    }
+
+    return temp;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  return 0;
+}
