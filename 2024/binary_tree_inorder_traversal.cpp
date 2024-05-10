85a2f6babf87f07c2e89329fa784ddf6affb69b7
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2024-05-10 10:43:50 -0400

    Binary Tree Inorder Traversal - easy - apple

---
+++ 2024/binary_tree_inorder_traversal.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+typedef long long int llint;
+typedef long long unsigned int ullint;
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  typedef TreeNode node;
+
+  vector<int> answer;
+
+  void __rec__(node* root) {
+    if (not root)
+      return;
+    __rec__(root->left);
+    answer.push_back(root->val);
+    __rec__(root->right);
+  }
+
+  vector<int> inorderTraversal(TreeNode* root) {
+    __rec__(root);
+    return answer;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  return 0;
+}
