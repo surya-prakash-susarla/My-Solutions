8dea76c66056e4514d73caa0cc82d240112bfbbd
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2024-05-10 10:39:30 -0400

    Search In Rotated Sorted Array - medium - apple

--- 2024/search_in_rotated_sorted_array.cpp
+++ 2024/search_in_rotated_sorted_array.cpp
@@ -21,38 +21,27 @@ using namespace std;
 
 class Solution {
  public:
+  int __rec__(int start, int end, const vector<int>& values, const int target) {
+    if (start > end)
+      return -1;
+
+    int mid = (start + end) / 2;
+    int midvalue = values[mid];
+    if (target == midvalue)
+      return mid;
+    if (midvalue > values[end] and (target > midvalue or target <= values[end]))
+      return __rec__(mid + 1, end, values, target);
+    if (midvalue < values[start] and
+        (target >= values[start] or target < midvalue))
+      return __rec__(start, mid - 1, values, target);
+    if (target < midvalue)
+      return __rec__(start, mid - 1, values, target);
+    else
+      return __rec__(mid + 1, end, values, target);
+  }
+
   int search(vector<int>& values, int target) {
-    int left = 0;
-    int right = values.size() - 1;
-    while (left <= right) {
-      int mid = (left + right) / 2;
-      if (values[mid] == target)
-        return mid;
-      if (values[left] <= values[mid]) {
-        if (target >= values[left] and target <= values[mid]) {
-          right = mid - 1;
-          continue;
-        }
-      } else {
-        if (target >= values[left] or target <= values[mid]) {
-          right = mid - 1;
-          continue;
-        }
-      }
-      if (values[right] >= values[mid]) {
-        if (target >= values[mid] and target <= values[right]) {
-          left = mid + 1;
-          continue;
-        }
-      } else {
-        if (target >= values[mid] or target <= values[right]) {
-          left = mid + 1;
-          continue;
-        }
-      }
-      break;
-    }
-    return -1;
+    return __rec__(0, values.size() - 1, values, target);
   }
 };
 
