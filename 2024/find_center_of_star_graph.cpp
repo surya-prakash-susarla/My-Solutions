3b6d52899c4677f5c1ec523790243e5a0c5ff9f5
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2024-06-26 17:17:54 -0700

    Find Center of Star Graph - easy - daily

---
+++ 2024/find_center_of_star_graph.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+typedef long long int llint;
+typedef long long unsigned int ullint;
+
+using namespace std;
+
+class Solution {
+ public:
+  int findCenter(vector<vector<int>>& input) {
+    int n = input.size() + 1;
+    map<int, int> temp;
+
+    for (const vector<int>& i : input) {
+      temp[i[0]]++;
+      temp[i[1]]++;
+
+      if (temp[i[0]] > 1)
+        return i[0];
+      if (temp[i[1]] > 1)
+        return i[1];
+    }
+
+    return -1;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  return 0;
+}
