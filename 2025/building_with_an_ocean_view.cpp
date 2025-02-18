086a238dab0a075a24799d4fdc1297e68ddc5ccb
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2025-02-17 23:23:59 -0800

    Building With an Ocean View - medium - meta

---
+++ 2025/building_with_an_ocean_view.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+typedef long long int llint;
+typedef long long unsigned int ullint;
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> findBuildings(vector<int>& input) {
+    int maxvalue = INT_MIN;
+    vector<int> answer;
+    for (int i = input.size() - 1; i > -1; i--) {
+      if (maxvalue < input[i]) {
+        answer.push_back(i);
+        maxvalue = input[i];
+      }
+    }
+    reverse(answer.begin(), answer.end());
+    return answer;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  return 0;
+}
