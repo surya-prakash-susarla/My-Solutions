b0f72691a20a1b8ff6d21cc1413a2cad12c61641
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2025-03-05 18:56:02 -0800

    Rotate Array - medium - meta

---
+++ 2025/rotate_array.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+typedef long long int llint;
+typedef long long unsigned int ullint;
+
+using namespace std;
+
+class Solution {
+ public:
+  void rotate(vector<int>& input, int k) {
+    k %= input.size();
+    vector<int> answer;
+    answer.insert(answer.end(), input.begin() + (input.size() - k),
+                  input.end());
+    answer.insert(answer.end(), input.begin(),
+                  input.begin() + (input.size() - k));
+    input = answer;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  return 0;
+}
