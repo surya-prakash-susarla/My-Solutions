541bf1f3342157dc207efcd78165f9f20a46c8bc
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2025-08-12 21:57:44 -0700

    Merge Sorted Array - easy - meta - loop

---
+++ 2025/merge_sorted_array.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+typedef long long int llint;
+typedef long long unsigned int ullint;
+
+using namespace std;
+
+class Solution {
+ public:
+  void merge(vector<int>& a, int m, vector<int>& b, int n) {
+    for (int i = m; i < m + n; i++)
+      a[i] = b[i - m];
+    sort(a.begin(), a.end());
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  return 0;
+}
