dc37b8ab01466713eea557bb40383cef783aa163
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2025-10-30 22:18:00 -0700

    Maximum Average Subtree - medium - random - tree - easy

---
+++ 2025/maximum_average_subtree.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+typedef long long int llint;
+typedef long long unsigned int ullint;
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  typedef TreeNode node;
+
+  double answer = 0;
+
+  pair<double, int> __rec__(node* root) {
+    if (!root)
+      return {0, 0};
+
+    pair<double, int> left = __rec__(root->left);
+    pair<double, int> right = __rec__(root->right);
+
+    pair<double, int> current;
+    current.first = left.first + right.first + root->val;
+    current.second = left.second + right.second + 1;
+
+    answer = max(answer, (current.first / current.second));
+
+    return current;
+  }
+
+  double maximumAverageSubtree(TreeNode* root) {
+    __rec__(root);
+    return answer;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  return 0;
+}
